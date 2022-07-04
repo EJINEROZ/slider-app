c01e8f50fb2b8411ddcb53b4fb065b3e9b9cbf20
UWEGHREPHA EJIRO THANKGOD <ejineroz@pop-os.localdomain>
2022-07-04 08:46:51 +0100

    an update to the slider-app

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -9,24 +9,24 @@ class Gallery extends React.Component {
                 <h2>My Photo Gallery</h2>
                 <Carousel autoPlay interval="5000" transitionTime="5000" infiniteLoop>
                     <div>
-                        <img src="https://images.unsplash.com/photo-1655847001912-20a783d10888?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80" alt="" />
+                        <img src="https://media.istockphoto.com/photos/concept-picture-id1154231467" alt="" />
                         <p className="legend">My Photo 1</p>
                     </div>
                     <div>
-                        <img src="https://images.unsplash.com/photo-1655826526409-6dd68d56cd78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80" alt="" />
+                        <img src="https://images.unsplash.com/photo-1656268164012-119304af0c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80" alt="" />
                         <p className="legend">My Photo 2</p>
                     </div>
                     <div>
-                        <img src="https://images.unsplash.com/photo-1605944615719-7a446e385fd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="" />
+                        <img src="https://images.unsplash.com/photo-1655745653127-4d6837baf958?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
                         <p className="legend">My Photo 3</p>
                     </div>
                     <div>
-                        <img src="https://images.unsplash.com/photo-1621609764095-b32bbe35cf3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=60" alt="" />
-                        <p className="legend">My Classic Still 3</p>
+                        <img src="https://images.unsplash.com/photo-1516527653392-602455dd9cf7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80" alt="" />
+                        <p className="legend">My Photo 4</p>
                     </div>
                     <div>
                         <img src="https://images.unsplash.com/photo-1655365225165-8d727fe3a091?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=80" alt="" />
-                        <p className="legend">My Photo 3</p>
+                        <p className="legend">My Photo 5</p>
                     </div>
                 </Carousel>
             </div>
